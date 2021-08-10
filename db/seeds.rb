2df7392051600862e1fffff6cfbd5d6da05f4cf9
meljun picardal <meljun.one@gmail.com>
2021-08-10 16:01:55 +0900

    created Article model and seeded Faker articles

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  title = Faker::Book.title
+  content = Faker::Quote.most_interesting_man_in_the_world
+  Article.create(title: title, content: content)
+end
